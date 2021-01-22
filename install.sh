355a48f133609b9825bae81cd1c8852909bee770
Nathan Walston <walston@users.noreply.github.com>
2021-01-22 11:23:11 -0800

    Announces debugging info

--- install.sh
+++ install.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# ZSH
-curl -fsSL "https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | bash;
+echo "# Installing ZSH" ;
+curl -fsSL "https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | bash ;
 
-# Homebrew
+echo "# Installing Homebrew" ;
 curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/master/install.sh" | bash \
-  && brew install deno nginx ripgrep vim;
+  && brew install deno nginx ripgrep vim ;
 
-# NVM
-curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh" | bash;
+echo "# Installing NVM" ;
+curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh" | bash ;
 
-# vim-plugin manager
+echo "# Installing vim-plugin manager"
 curl -fLo "~/.vim/autoload/plug.vim" --create-dirs \
-    "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
+    "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" ;
 
 # OS setup
 # Copies OS setup stuff;
-let DOTFILES_DIR="~/Repos/dotfiles"
-git clone git@github.com:walston/dotfiles.git $DOTFILES_DIR;
+let DOTFILES_DIR="~/Repos/dotfiles" ;
+echo "Installing walston/dotfiles into $DOTFILES_DIR" ;
+git clone git@github.com:walston/dotfiles.git $DOTFILES_DIR ;
 
-ln -nsf $DOTFILES_DIR/.zshrc ~/.zshrc ;
-ln -nsf $DOTFILES_DIR/.profile ~/.profile ;
-ln -nsf $DOTFILES_DIR/.bash_profile ~/.bash_profile ;
-ln -nsf $DOTFILES_DIR/.vimrc ~/.vimrc;
+ln -nsfv $DOTFILES_DIR/.zshrc ~/.zshrc ;
+ln -nsfv $DOTFILES_DIR/.profile ~/.profile ;
+ln -nsfv $DOTFILES_DIR/.bash_profile ~/.bash_profile ;
+ln -nsfv $DOTFILES_DIR/.vimrc ~/.vimrc;
   
 vim +PluginInstall +qall ;
   
