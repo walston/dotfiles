1cd2aaf23137be5b9d5fb57008fea7df85e8f374
Nathan Walston <walston@users.noreply.github.com>
2021-01-22 11:18:08 -0800

    overwrite links so I can "reinstall"

--- install.sh
+++ install.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
+command_exists() {
+  if command -v $1 &> /dev/null; then
+    return 1;
+  fi
+}
 
 # ZSH
-curl -fsSL "https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | bash
+if [ command_exists zsh ]; then
+curl -fsSL "https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | bash;
 
 # Homebrew
 curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/master/install.sh" | bash \
-  && brew install ctags deno nginx python ripgrep vim
+  && brew install ctags deno nginx python ripgrep vim;
 
 # NVM
-curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh" | bash
+curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh" | bash;
 
 # vim-plugin manager
 curl -fLo "~/.vim/autoload/plug.vim" --create-dirs \
-    "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+    "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
 
 # OS setup
 # Copies OS setup stuff;
-git clone git@github.com:walston/dotfiles.git ~/Repos/dotfiles \
-  && cd ~/Repos/dotfiles \
-  && ln -s ~/Repos/dotfiles/.zshrc ~/.zshrc \
-  && ln -s ~/Repos/dotfiles/.profile ~/.profile \
-  && ln -s ~/Repos/dotfiles/.bash_profile ~/.bash_profile \
-  && ln -s ~/Repos/dotfiles/.vimrc ~/.vimrc && vim +PluginInstall +qall \
-  ;
+let DOTFILES_DIR="~/Repos/dotfiles"
+git clone git@github.com:walston/dotfiles.git $DOTFILES_DIR;
+
+ln -nsf $DOTFILES_DIR/.zshrc ~/.zshrc ;
+ln -nsf $DOTFILES_DIR/.profile ~/.profile ;
+ln -nsf $DOTFILES_DIR/.bash_profile ~/.bash_profile ;
+ln -nsf $DOTFILES_DIR/.vimrc ~/.vimrc;
+  
+vim +PluginInstall +qall ;
+  
 
