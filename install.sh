cdef56ffb4f320d4f93e8c453c2c2974e363b2b0
Nathan Walston <walston@users.noreply.github.com>
2024-07-25 21:19:48 -0700

    Updates the install script as best I could

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ ! type brew 2>&1 >/dev/null ]; then
+if [[ $(which -s brew) == "" ]] ; then
   echo "# Installing Homebrew" ;
   curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/master/install.sh" | bash ;
-  brew install git-delta httpie jq neovim nginx ripgrep wget zsh ;
 else
   echo "# Homebrew already installed" ;
 fi
+brew install git-delta httpie jq neovim nginx pure ripgrep wget zsh ;
 
-if [[ ! -d "$HOME/.nvm" ]]; then
+if [[ ! -d "$HOME/.nvm" ]] ; then
   echo "# Installing NVM" ;
   curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh" | bash ;
 else
@@ -18,7 +18,7 @@ fi
 # OS setup
 # Copies OS setup stuff;
 DOTFILES_DIR="$HOME/Repos/dotfiles" ;
-if [ -d "$DOTFILES_DIR" ]; then
+if [[ -d "$DOTFILES_DIR" ]] ; then
   echo "Pulling walston/dotfiles in $DOTFILES_DIR";
   git -C "$DOTFILES_DIR" pull origin $(git -C "$DOTFILES_DIR" rev-parse --abbrev-ref HEAD);
 else
@@ -29,15 +29,14 @@ else
   git -C "$DOTFILES_DIR" branch checkout -b "$name"
 fi
 
-ln -nsfv "$DOTFILES_DIR/.zshrc"         "$HOME/.zshrc" ;
+ln -nsfv "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc" ;
 
 CONFIG_NEOVIM_DIR="$HOME/.config/nvim"
-if [[ ! -d "$CONFIG_NEOVIM_DIR" ]]; then
-  echo "# Installing "
-  git clone --depth 1 https://github.com/AstroNvim/template "$CONFIG_NEOVIM_DIR"
-  rm -rf ~/.config/nvim/.git
+if [[ ! -d "$CONFIG_NEOVIM_DIR" ]] ; then
+  echo "# Installing AstroNvim"
+  git clone git@github.com:walston/nvim.git "$CONFIG_NEOVIM_DIR"
 else
   echo "# Updating AstroNvim"
-  git -C "$CONFIG_NEOVIM_DIR" pull --depth 1
+  git -C "$CONFIG_NEOVIM_DIR" pull
 fi
 
