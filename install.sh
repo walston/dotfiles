5ef121b577803ac31980b60b95d450120e8c999c
Nathan Walston <walston@users.noreply.github.com>
2024-07-25 17:33:38 -0700

    updates the install script, drops unused files

--- install.sh
+++ install.sh
@@ -4,15 +4,8 @@ echo "# Installing Homebrew" ;
 curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/master/install.sh" | bash \
   && brew install git-delta httpie jq nginx ripgrep vim wget zsh;
 
-echo "# Installing ZSH" ;
-curl -fsSL "https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | bash ;
-
 echo "# Installing NVM" ;
 curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh" | bash ;
-
-echo "# Installing vim-plugin manager"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # OS setup
 # Copies OS setup stuff;
@@ -25,10 +18,3 @@ else
 fi
 
 ln -nsfv "$DOTFILES_DIR/.zshrc"         "$HOME/.zshrc" ;
-ln -nsfv "$DOTFILES_DIR/.profile"       "$HOME/.profile" ;
-ln -nsfv "$DOTFILES_DIR/.bash_profile"  "$HOME/.bash_profile" ;
-ln -nsfv "$DOTFILES_DIR/.vimrc"         "$HOME/.vimrc" ;
-
-vim +PluginInstall +qall ;
-
-
